ea4d29663d6701e3aa9bf0a4a38a80eb56c15e6e
Alex Mason <alexmason528@hotmail.com>
2017-09-27 08:33:48 -0400

    community

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -26,14 +26,72 @@ export default class HomePage extends Component { // eslint-disable-line react/p
         <div className="row">
           <div className="column profile-column">
             <div className="content">
+              <div className="tile">
+                <div className="profile">
+                  <img src="https://carta.guide/image/wide/0025.jpg" role="presentation" />
+                  <div className="profile-pic">
+                    <img src="https://carta.guide/image/profile/bag/0.jpg" role="presentation" />
+                  </div>
+                  <h2>Sign in</h2>
+                </div>
+              </div>
+              <div className="tile">
+                <div className="auth-form">
+                </div>
+              </div>
+              <div className="tile">
+                <div className="start-quest">
+                  <img src="https://carta.guide/image/quest/start/1.jpg" role="presentation" />
+                  <h2>Start your personal quest</h2>
+                </div>
+              </div>
             </div>
           </div>
           <div className="column wall-column">
             <div className="content">
+              <div className="tile">
+                <div className="post">
+                  <img src="https://carta.guide/image/wide/0012.jpg" role="presentation" />
+                  <h2>Find your favorite weather</h2>
+                </div>
+                <div className="description">
+                  <div className="heading">
+                    <span className="title">MARTIJN SNELDER - CARTA</span>
+                    <span className="date"> | 1 day ago</span>
+                  </div>
+                  <p className="content">
+                    We added climate and weather information to our database! So start searching for sunshine, snow, heat, or wind. Whatever you prefer!
+                  </p>
+                </div>
+              </div>
+
+              <div className="tile">
+                <div className="post">
+                  <img src="https://carta.guide/image/wide/0002.jpg" role="presentation" />
+                  <h2>Iran</h2>
+                </div>
+                <div className="description">
+                  <p className="content">
+                    {"Photos of Iran. This doesn't include people portraits. Those photos are reserved for the printed album I'm going to make."}
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
           <div className="column various-column">
             <div className="content">
+              <div className="tile">
+                <div className="poster">
+                  <img src="https://carta.guide/image/0027.jpg" role="presentation" />
+                  <h2>What are your favorite coffee spots?</h2>
+                </div>
+              </div>
+              <div className="tile">
+                <div className="poster">
+                  <img src="https://carta.guide/image/0061.jpg" role="presentation" />
+                  <h2>Best dinners in Amsterdam</h2>
+                </div>
+              </div>
             </div>
           </div>
         </div>
